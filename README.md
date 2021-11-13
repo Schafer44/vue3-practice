842288a3b261a7c67609e26057ea94d3d1681aad
Schafer44 <tanner44schafer@gmail.com>
2021-11-12 19:41:40 -0600

    Learning vue3 from crash course details in readme

--- README.md
+++ README.md
@@ -1,19 +1,32 @@
 # vue-practice
 
+This was my first time working with vue3. This app was originally made by Traversy Media on youtube.
+Vue JS Crash Course 2021
+https://www.youtube.com/watch?v=qZXt1Aom3Cs
+Plan on writing my own version over the next few days
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
+both
+npm run backend
+&
 npm run serve
+are required to get a working dev server ups
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      v-show="homePage"
+      @btn-click="$emit('toggle-add-task')"
+      :text="ShowAddTask ? 'Close' : 'Add Task'"
+      :color="ShowAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    ShowAddTask: Boolean,
+  },
+  components: { Button },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") return true;
+      else return false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
